51550c18ffb7d26a196745d1349ced646c585356
ppcharania <ppcharania@gmail.com>
2024-05-04 11:29:16 +0530

    Responsve and Animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Hero.css'
 import {HiLocationMarker} from 'react-icons/hi'
 import CountUp from "react-countup"
-
+import { motion } from 'framer-motion'
 const Hero = () => {
     return (
         <section className="herowrapper">
@@ -10,12 +10,12 @@ const Hero = () => {
                 <div className="flexColStart hero-left">
                     <div className="hero-title">
                         <div className="orange-circle"></div>
-                        <h1>
+                        <motion.h1 initial={{y:"2rem", opacity:0}} animate={{y:0, opacity:1 }} transition={{duration:2, type:"ease-in"}}>
                             Discover <br />
                             Most suitable <br />
                             Property
 
-                        </h1>
+                        </motion.h1>
 
                     </div>
 
@@ -59,9 +59,9 @@ const Hero = () => {
                 </div>
 
                 <div className="flexCenter hero-right">
-                    <div className="image-container">
+                    <motion.div className="image-container" initial={{x:"7rem", opacity:0}} animate={{x:0, opacity:1 }} transition={{duration:2, type:"ease-in"}}>
                         <img src="./hero-image.png" alt="" />
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </section >
